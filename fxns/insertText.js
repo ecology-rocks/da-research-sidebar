44594ec9891cb77f121c93c524aa76e5a6ffc118
Sam L. S. Davis, Ph.D <ecology-rocks@users.noreply.github.com>
2018-10-03 09:44:57 -0400

    updates and code cleanup / deduplication

--- fxns/insertText.js
+++ fxns/insertText.js
@@ -32,8 +32,10 @@ function askAboutInsertSolo(myString){
  function insertTextAtCursor(myArr){     
       var cursor = DocumentApp.getActiveDocument().getCursor(); 
    
+    if(!Array.isArray(myArr)){ myArr = [myArr]; };
+   
       if (cursor) {
-        var element = cursor.insertText(myArr.join('\n\n'));
+        var element = cursor.insertText('\n' + myArr.join('\n\n') + '\n');
         if (!element) {
           DocumentApp.getUi().alert('Cannot insert text here, sorry!');
         }
@@ -42,6 +44,75 @@ function askAboutInsertSolo(myString){
       }        
     }
 
+
+/***********************
+   CITATION FUNCTIONS
+************************/
+function insertCiteAtCursor(citeText, citeLink, linkText){
+
+  //if the link text is blank, set it to the link URL.
+  //later on, if the link is not blank, it will insert as a link.
+     if(linkText.length == 0){
+       linkText = citeLink;
+     }
+  
+    var cursor = DocumentApp.getActiveDocument().getCursor(); 
+      if (cursor) {
+
+         if(citeLink.length > 0){
+           cursor.insertText('\n');
+           cursor.insertText(linkText).setLinkUrl(citeLink);
+         }
+       cursor.insertText('\n'  + citeText + ' ');
+
+        if (!element) {
+          DocumentApp.getUi().alert('Cannot insert text here, sorry!');
+        }
+      } else {
+        DocumentApp.getUi().alert('Cannot find a cursor, sorry! You can\'t have text highlighted while trying to insert.');
+      }
+};
+
+function insertCiteFromLib(citeKey){
+  
+  //loading the spreadsheet
+   var spreadsheetId = '14M5jm7oFJHFDQ9Xgf9PqchUaZIJ2DJWl6Pjb0rp2SDc';
+   var rangeName = 'citations!A2:D';
+   var values = Sheets.Spreadsheets.Values.get(spreadsheetId, rangeName).values;
+  
+  //blank data
+   var data = {};
+  
+  //loop through citations spreadsheet
+    for (var row = 0; row < values.length; row++) {      
+//if we have a hit, put it in data
+     if(values[row][0] == citeKey){
+        data = {
+           cite: values[row][1],
+            url: values[row][2], 
+       linkText: values[row][3]
+     };
+    }//end if
+  }//end for
+  
+  //if there were no hits, send error message instead
+    if(!data.hasOwnProperty('cite')){
+        var data = {
+            cite: 'This citation does not exist',
+            url: '', 
+            linkText: ''
+     };
+       };
+  //insert text as citation
+  insertCiteAtCursor(data.cite, data.url, data.linkText);
+  
+};
+
+
+
+/***********************************
+  INSERTING OTHER MEDIA TYPES
+***********************************/
 
 
  function insertLinkAtCursor(link, title){     
@@ -52,7 +123,9 @@ function askAboutInsertSolo(myString){
    }
    
       if (cursor) {
+        var beginning = cursor.insertText('\n');
         var element = cursor.insertText(title);
+        var end = cursor.insertText('\n');
         element.setLinkUrl(link);
         if (!element) {
           DocumentApp.getUi().alert('Cannot insert text here, sorry!');
@@ -63,25 +136,89 @@ function askAboutInsertSolo(myString){
     }
 
 
-//sample function to play with
 function insertImage(img) {
 
   // Insert a web image  
-  var img = "https://img.labnol.org/logo.png";
+  //var img = "https://img.labnol.org/logo.png";
   var blob = UrlFetchApp.fetch(img).getBlob();
   
   // Insert an image from Google Drive
-  var img  = "https://drive.google.com/open?id=xyz";
-  var fileID = img.match(/[\w\_\-]{25,}/).toString();
-  var blob   = DriveApp.getFileById(fileID).getBlob();
+  //var img  = "https://drive.google.com/open?id=xyz";
+ // var fileID = img.match(/[\w\_\-]{25,}/).toString();
+ // var blob   = DriveApp.getFileById(fileID).getBlob();
   
   var doc = DocumentApp.getActiveDocument();
   var cursor = doc.getCursor();
   
   if (cursor) {    
-    cursor.insertInlineImage(blob);
+    var myImage = cursor.insertInlineImage(blob);
   } else {
-    doc.getBody().insertImage(0, image);
+    var myImage = doc.getBody().insertImage(0, image);
   }
   
+var width = myImage.getWidth();
+var newW = width;
+var height = myImage.getHeight();
+var newH = height;
+var ratio = width/height
+
+if(width>640){
+newW = 640;
+newH = parseInt(newW/ratio);
 }
+myImage.setWidth(newW).setHeight(newH)
+
+  
+}
+
+//https://stackoverflow.com/questions/19987101/how-to-copy-content-and-formatting-between-google-docs
+//https://stackoverflow.com/questions/19894885/how-to-copy-one-or-more-existing-pages-of-a-document-using-google-apps-script/19907943#19907943
+//insert document function to play with
+function importInDoc(documentid) {
+
+  //source document for copying
+  var sourceDoc = DocumentApp.openById(documentid).getBody();
+  
+  //get this document
+  var targetDoc = DocumentApp.getActiveDocument();
+
+  var totalElements = sourceDoc.getNumChildren();
+
+  for( var j = 0; j < totalElements; ++j ) {
+    var body = targetDoc.getBody()
+    var element = sourceDoc.getChild(j).copy();
+    var type = element.getType();
+    if( type == DocumentApp.ElementType.PARAGRAPH ){
+      body.appendParagraph(element);
+    }
+    else if( type == DocumentApp.ElementType.TABLE){
+      body.appendTable(element);
+      }
+    else if( type == DocumentApp.ElementType.LIST_ITEM){
+      body.appendListItem(element);
+      }
+//    ...add other conditions (headers, footers...
+    }
+  targetDoc.saveAndClose();
+}
+
+
+/**************************
+Specific Insertion Functions
+***************************/
+
+function getFact(recordNum){
+//data is coming from UserProperty
+ var data = getPageData();
+ var insertMe = data.facts[recordNum];
+  //wrapped in array, be sure to just send string.
+  if(!Array.isArray(insertMe)){ insertMe = [insertMe]; };
+  insertTextAtCursor(insertMe);
+  
+}
+
+
+
+
+
+
--- fxns/quickinserts.js
+++ fxns/quickinserts.js
@@ -1,3 +1,57 @@
 function daMission() {
   askAboutInsert(['Dogwood Alliance mobilizes diverse voices to protect southern forests and communities from destructive industrial logging.']);
 }
+
+function insertHeader() {
+  var header =  '<!DOCTYPE html><html>' + 
+    '<head><base target="_top">' + 
+      '<link rel="stylesheet" href="https://ssl.gstatic.com/docs/script/css/add-ons1.css">'+
+        '<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">' +      
+          '<link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">' + 
+            '</head><body>' + 
+              ' <div class="m-2 p-2 text-center"><span style="font-family: \'Lobster Two\', cursive;"><img src="https://i.imgur.com/9WeUrTo.png"><b>Sam\'s Research Menu</b> &nbsp; &nbsp; </span>';
+  header += insertNav();
+  header += '</div></div>';
+  return header;
+}
+
+function insertFooter() { 
+  var footer = '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>' +
+    '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>' +
+      '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>' + 
+        '</body></html>';
+  return footer;
+}
+
+
+
+
+function insertNav(){
+  var navlist = '<button class="action btn-sm" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Navigation</button>' +
+    '<div class="collapse p-2 m-2" id="collapseExample">';
+  var amDataKeys = getDataKeys(appMap);
+  
+  for(var i = 0; i < amDataKeys.length; i++){
+    
+    var title = appMap[amDataKeys[i]].title;
+    
+    navlist += '<small><b>' + title + '</b></small>: ';
+    //for the j loop
+    var items = appMap[amDataKeys[i]].items;
+    var itemDataKeys = getDataKeys(items);
+    
+    
+    for(var j = 0; j < itemDataKeys.length; j++){
+      navlist += '<small><a href="#" onClick="google.script.run.' + itemDataKeys[j] + '();return false;">' + items[itemDataKeys[j]] + ' ' + '</a></small>';
+      if(j < itemDataKeys.length - 1){
+        navlist += " - ";
+      }
+    } // end j loop
+    navlist += '<br />';
+    
+    
+  }//end i loop
+  navlist += '';
+  
+  return navlist;
+}
